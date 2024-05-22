0b3c3e734a514570ca1706924f40a46669b54fb8
CarolineMagnenat <145575566+CarolineMagnenat@users.noreply.github.com>
2024-05-22 15:30:07 +0200

    tjena i login

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,44 +1,13 @@
-import React from "react";
-import { StyleSheet, Text, View, Button, TextInput } from "react-native";
+import { StyleSheet, Text, View, Button, ScrollView } from "react-native";
 
-const LoginScreen = ({ navigation }) => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Login</Text>
-      <TextInput style={styles.input} placeholder="Username" />
-      <TextInput style={styles.input} placeholder="Password" secureTextEntry />
-      <Button
-        title="Login"
-        onPress={() => {
-          /* Implement login logic */
-        }}
-      />
-      <Button
-        title="Register"
-        onPress={() => navigation.navigate("Register")}
-      />
-    </View>
-  );
-};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    paddingHorizontal: 20,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-    textAlign: "center",
-  },
-  input: {
-    height: 40,
-    borderColor: "gray",
-    borderWidth: 1,
-    marginBottom: 20,
-    paddingHorizontal: 10,
-  },
-});
+const LoginScreen = () => {
+
+    return (
+        <>
+            <Text>TJEEENA</Text>
+        </>
+    )
+}
 
 export default LoginScreen;
